8dee751d3c1d55a2bd5e4a69edc6d6174cb050f3
Akriti <akritikohli05@gmail.com>
2024-09-05 12:31:34 +0530

    used and practiced Scss!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    }, 
+};
 
 export default nextConfig;
--- src/app/themeContextApi/page.tsx
+++ src/app/themeContextApi/page.tsx
@@ -1,14 +1,21 @@
 "use client"
 import React, { useContext } from 'react'
 import { ThemeContext } from '../component/ThemeContext'
-
+import "@/styles/app.scss"
 const UseContextSample = () => {
   const {state , dispatch} = useContext(ThemeContext);
   console.log(state)
   return (
-    <div className='w-full h-[88vh] flex flex-col justify-center items-center'>
+    <div>
+    <div className='w-full h-[40vh] flex flex-col justify-center items-center'>
       <button className='bg-gradient-to-tr from-pink-700 to-blue-700 text-white p-4 rounded-lg mb-4' onClick={() => dispatch({type:"ChangeTheme"})}>Change Theme</button>
       <button className='bg-gradient-to-tr from-gray-600 to-green-700 text-white p-4 rounded-lg mb-4'onClick={() => dispatch({type:"ChangeFont" , payload:20})}>Change Font</button>
+    </div>
+    <div className='container'>
+      <button className='button_Sass' >Hey Sass button!</button>
+      <h1>Hey Sass heading</h1>
+      <p>Hey Sass pTag</p>
+    </div>
     </div>
   )
 }
